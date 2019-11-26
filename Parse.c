7b37cc3c383409b0e6a49cab8480fa5264518808
Isabella Rocha <irocha00@stuy.edu>
2019-11-26 15:35:24 -0500

    End of class

--- Parse.c
+++ Parse.c
@@ -9,12 +9,25 @@
 #include <sys/types.h>
 
 char ** parse_args( char * line ) {
-    char** = args[6];
+    char** args;
+    char* curr = line;
     int idx = 0;
     char *token;
-    while(line != NULL) {
-        token = strsep(&line, " ");
+    while(curr != NULL) {
+        token = strsep(&curr, " ");
         args[idx] = token;
     }
     return args;
+}
+
+void printArray(char ** args) {
+    int idx = 0;
+    for (idx; idx < sizeof(args); idx++) {
+        printf("%d ", args[idx]);
+    }
+}
+
+int main() {
+    printArray(parse_args("ls -a -l"));
+    return 0;
 }
